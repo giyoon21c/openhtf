d07064207f23c9bc8f3114446c16bdb78c056447
Giyoung Yoon <giyoon@google.com>
2021-05-29 19:00:31 -0700

    some more example test

---
+++ examples/arduino_test_v0.py
@@ -0,0 +1,10 @@
+import openhtf as htf
+
+@htf.measures(htf.Measurement("P4_digital")
+              .doc("Pin 4 Digital Measurement")
+              .equals(True))
+def digital_read(test):
+    test.measurements.P4_digital = False
+
+test = htf.Test(digital_read)
+test.execute()
